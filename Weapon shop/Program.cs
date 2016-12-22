05a15059e3ffdb0b9f01c44b4feda7c6cdba9e51
Nawaz <Niloy.shah@hotmail.com>
2016-12-23 03:11:09 +0600

    last

--- Weapon shop/Program.cs
+++ Weapon shop/Program.cs
@@ -16,7 +16,7 @@ namespace Weapon_shop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Login());
+            Application.Run(new POS());
         }
     }
 }
