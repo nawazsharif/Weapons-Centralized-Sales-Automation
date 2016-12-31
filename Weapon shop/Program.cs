b787c94b364e0a67c01dbcaed6164c5b1039a438
Nawaz <Niloy.shah@hotmail.com>
2016-12-31 22:22:53 +0600

    Staff Fix all

--- Weapon shop/Program.cs
+++ Weapon shop/Program.cs
@@ -16,7 +16,7 @@ namespace Weapon_shop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Staff());
         }
     }
 }
